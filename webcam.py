82e355d0227d0040976e18e10b9f61823f17eca7
Adharsh M <adharsh.m@vu.edu.au>
2023-12-30 19:15:00 +0530

    Added webcam flask integration.

--- webcam.py
+++ webcam.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, Response
+import face_recognition
+import numpy as np
 import cv2
+import os
 
 app = Flask(__name__)
 
@@ -9,18 +12,83 @@ app = Flask(__name__)
 # camera = cv2.VideoCapture(0)
 
 camera = cv2.VideoCapture(0)
+# Directory containing images
+image_dir = "images"
+# Lists to hold face encodings and names
+known_face_encodings = []
+known_face_names = []
+
+# Loop over each file in the directory
+for filename in os.listdir(image_dir):
+    # Only process files with .jpeg or .jpg extension
+    if filename.endswith(".jpeg") or filename.endswith(".jpg") or filename.endswith(".JPG") or filename.endswith(".png") or filename.endswith(".PNG"):
+        # Load image
+        image = face_recognition.load_image_file(os.path.join(image_dir, filename))
+        # Get face encodings for the image
+        face_encodings = face_recognition.face_encodings(image)
+        if face_encodings:
+            face_encoding = face_encodings[0]
+            # Add face encoding to list
+            known_face_encodings.append(face_encoding)
+            # Add name to list. We remove the file extension to get the name
+            known_face_names.append(os.path.splitext(filename)[0])
+        else:
+            print(f"No faces found in the image {filename}")
 
 def gen_frames():  # generate frame by frame from camera
+    process_this_frame = True
     while True:
         # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
+            if process_this_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+                # Find all the faces and face encodings in the current frame of video
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
+
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+
+                    face_names.append(name)
+            process_this_frame = not process_this_frame
+            
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (255, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/video_feed')
@@ -36,4 +104,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3003)
+    app.run(debug=True, port=3003, host='0.0.0.0')
